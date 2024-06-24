dcd285c3ab817572a3d57b9e20c2621622bc3f12
Diniru Karunarathne <99533573+DiniruKarunarathne@users.noreply.github.com>
2024-06-24 15:57:09 +0530

    Added sendEmail function to handle form submission with emailjs
    Added sendEmail function to handle form submission with emailjs

--- src/Sections/Contact/Contact.jsx
+++ src/Sections/Contact/Contact.jsx
@@ -1,10 +1,18 @@
 import styles from './ContactStyles.module.css';
+import emailjs from '@emailjs/browser';
 
 function Contact() {
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    alert('Email sent!');
+
+    emailjs.sendForm('service_e1y5147', 'template_nn5re05', e.target, 'ONrOOq12q2ECPqVSx')
+  }
   return (
     <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
-      <form action="">
+      <form onSubmit={sendEmail}>
         <div className="formGroup">
           <label htmlFor="name" hidden>
             Name
